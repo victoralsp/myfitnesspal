64116456a9b70d929b8daaf7394e5e68a25370ca
Victor Hugo Alves Pinheiro <victorpinheiro1008@gmail.com>
2024-04-10 19:41:30 -0300

    trocando switch por if else

--- index.js
+++ index.js
@@ -10,22 +10,19 @@ const calcularIMC = () => {
          const imc = (peso.value / (altura.value * altura.value)).toFixed(2)
         let classificacao = ''
 
-        switch (imc) {
-            case "< 18.5": 
+        // Verificar a classificação do IMC
+        if (imc < 18.5) {
             classificacao = 'Abaixo do peso'
-            break
-            case "< 25": 
-            classificacao = 'Peso normal'
-            break
-            case "< 30": 
-            classificacao = 'A cima do peso'
-            break
-            case "< 40":
+        } else if (imc < 25) {
+            classificacao = 'Peso Normal'
+        } else if (imc < 30) {
+            classificacao = 'Acima do peso'
+        } else if (imc < 40) {
             classificacao = 'Obesidade grau II'
-            break
-            case "<= 40":
+        } else {
             classificacao = 'Obesidade grau III'
         }
+
 
         // Exibir resultado na tela 
         resultado.innerHTML = `IMC: ${imc} (${classificacao})`
