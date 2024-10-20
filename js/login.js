b468d44a550f30a1df022434a5b5b2bd12e71a0c
Victor Hugo Alves Pinheiro <victorpinheiro1008@gmail.com>
2024-10-20 20:07:19 -0300

    adição de icone para fechar menu e ajustes

--- js/login.js
+++ js/login.js
@@ -5,8 +5,13 @@ let menu = true
 menuIcon.addEventListener('click', () => {
     if(menu) {
         navBar.style.display = 'block'
+        navBar.style.transform = "translateX(0)"
+        menuIcon.innerHTML = '<i class="fa-solid fa-xmark"></i>'
+        
     } else {
         navBar.style.display = 'none'
+        navBar.style.transform = "translateX(-100%)"
+        menuIcon.innerHTML = '<i class="fa-solid fa-bars">'
     }
     menu = !menu
 })
