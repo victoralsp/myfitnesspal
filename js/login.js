c82eeaf23c55a1d6b0f7b5cf05a2d56c66f5edb7
Victor Hugo Alves Pinheiro <victorpinheiro1008@gmail.com>
2024-10-20 20:20:45 -0300

    alterei de display para opacity

--- js/login.js
+++ js/login.js
@@ -4,12 +4,12 @@ let menu = true
 
 menuIcon.addEventListener('click', () => {
     if(menu) {
-        navBar.style.display = 'block'
+        navBar.style.opacity = '100'
         navBar.style.transform = "translateX(0)"
         menuIcon.innerHTML = '<i class="fa-solid fa-xmark"></i>'
         
     } else {
-        navBar.style.display = 'none'
+        navBar.style.opacity = '0'
         navBar.style.transform = "translateX(-100%)"
         menuIcon.innerHTML = '<i class="fa-solid fa-bars">'
     }
