ad4bb2fd5fac1b0e64aa322a8b331bfddd03f5a9
Victor Hugo Alves Pinheiro <victorpinheiro1008@gmail.com>
2024-10-21 21:59:43 -0300

    Adicionando modo noturno na pagina principal

--- js/index.js
+++ js/index.js
@@ -1,16 +1,12 @@
+// Section de calcular imc 
 const resultado = document.querySelector('#resultado')
 const altura = document.querySelector('#altura')
 const peso = document.querySelector('#peso')
 
-
-// Função para calcular o IMC
 const calcularIMC = () => {
     if (altura.value !== '' && peso.value !== '') {
-        // Fórmula do IMC
          const imc = (peso.value / (altura.value * altura.value)).toFixed(2)
         let classificacao = ''
-
-        // Verificar a classificação do IMC
         if (imc < 18.5) {
             classificacao = 'Abaixo do peso'
         } else if (imc < 25) {
@@ -23,11 +19,58 @@ const calcularIMC = () => {
             classificacao = 'Obesidade grau III'
         }
 
-
-        // Exibir resultado na tela 
         resultado.innerHTML = `IMC: ${imc} (${classificacao})`
         }
         else {
-            resultado.innerHTML = 'Preencha os campos!'
+            resultado.innerHTML = 'Verifique se os campos foram preenchidos corretamente.'
         }
 }
+
+// Modo noturno 
+
+const body = document.body
+const footer = document.getElementById('footer')
+const header = document.getElementById('header')
+const main = document.getElementById('main')
+const btnNoturno = document.getElementById('btnNoturno')
+const login = document.getElementById('login')
+const titleComece = document.getElementById('title-comece')
+const bgColor = document.getElementById('bg-color')
+const paragrafoComece = document.getElementById('paragrafo-comece')
+const subtitleResgistre = document.getElementById('subtitle-registre')
+const paragrafoRegistre = document.getElementById('paragrafo-registre')
+const subtitleFerramentas = document.getElementById('subtitle-ferramentas')
+const paragrafoFerramentas = document.getElementById('paragrafo-ferramentas')
+let noturno = false
+
+btnNoturno.addEventListener('click', () => {
+    if (noturno) {
+        header.style.backgroundColor  = 'var(--cor-branco)'
+        header.style.boxShadow = '2px 2px 8px rgba(0, 0, 0, 0.070)'
+        main.style.backgroundColor  = 'var(--cor-branco)'
+        footer.style.backgroundColor  = 'var(--cor-branco)'
+        login.style.color = 'var(--cor-dark)'
+        titleComece.style.color = 'var(--cor-dark)'
+        paragrafoComece.style.color = 'var(--cor-dark)'
+        subtitleResgistre.style.color = 'var(--cor-dark)'
+        paragrafoRegistre.style.color = 'var(--cor-dark)'
+        subtitleFerramentas.style.color = 'var(--cor-dark)'
+        paragrafoFerramentas.style.color = 'var(--cor-dark)'
+        bgColor.style.backgroundColor = 'var(--cor-branco)'
+        
+    } else {
+        header.style.backgroundColor  = 'var(--cor-dark)'
+        header.style.boxShadow = '2px 2px 10px #f7f7f720'
+        main.style.backgroundColor  = 'var(--cor-dark)'
+        footer.style.backgroundColor  = 'var(--cor-dark)'
+        login.style.color = 'var(--cor-branco)'
+        titleComece.style.color = 'var(--cor-branco)'
+        paragrafoComece.style.color = 'var(--cor-branco)'
+        subtitleResgistre.style.color = 'var(--cor-branco)'
+        paragrafoRegistre.style.color = 'var(--cor-branco)'
+        subtitleFerramentas.style.color = 'var(--cor-branco)'
+        paragrafoFerramentas.style.color = 'var(--cor-branco)'
+        bgColor.style.backgroundColor = 'var(--cor-dark)'
+    }
+    noturno = !noturno
+})
