64504507b67b893a158e6c82769856cb69d073e7
Victor Hugo Alves Pinheiro <victorpinheiro1008@gmail.com>
2024-10-21 22:05:46 -0300

    Forma melhor de fazer o modo noturno, redução de códigos e mais simples.

--- js/index.js
+++ js/index.js
@@ -28,49 +28,41 @@ const calcularIMC = () => {
 
 // Modo noturno 
 
-const body = document.body
-const footer = document.getElementById('footer')
-const header = document.getElementById('header')
-const main = document.getElementById('main')
-const btnNoturno = document.getElementById('btnNoturno')
-const login = document.getElementById('login')
-const titleComece = document.getElementById('title-comece')
-const bgColor = document.getElementById('bg-color')
-const paragrafoComece = document.getElementById('paragrafo-comece')
-const subtitleResgistre = document.getElementById('subtitle-registre')
-const paragrafoRegistre = document.getElementById('paragrafo-registre')
-const subtitleFerramentas = document.getElementById('subtitle-ferramentas')
-const paragrafoFerramentas = document.getElementById('paragrafo-ferramentas')
-let noturno = false
+const btnNoturno = document.getElementById('btnNoturno');
+const header = document.getElementById('header');
+const main = document.getElementById('main');
+const footer = document.getElementById('footer');
+const login = document.getElementById('login');
+const titleComece = document.getElementById('title-comece');
+const bgColor = document.getElementById('bg-color');
+const paragrafoComece = document.getElementById('paragrafo-comece');
+const subtitleResgistre = document.getElementById('subtitle-registre');
+const paragrafoRegistre = document.getElementById('paragrafo-registre');
+const subtitleFerramentas = document.getElementById('subtitle-ferramentas');
+const paragrafoFerramentas = document.getElementById('paragrafo-ferramentas');
+
+let noturno = false;
+
+const modoNoturno = () => {
+    const cor = noturno ? 'dark' : 'branco';
+    const textoCor = noturno ? 'branco' : 'dark';
+
+    header.style.backgroundColor = `var(--cor-${cor})`;
+    header.style.boxShadow = noturno ? '2px 2px 10px #f7f7f720' : '2px 2px 8px rgba(0, 0, 0, 0.070)';
+    main.style.backgroundColor = `var(--cor-${cor})`;
+    footer.style.backgroundColor = `var(--cor-${cor})`;
+    login.style.color = `var(--cor-${textoCor})`;
+    titleComece.style.color = `var(--cor-${textoCor})`;
+    paragrafoComece.style.color = `var(--cor-${textoCor})`;
+    subtitleResgistre.style.color = `var(--cor-${textoCor})`;
+    paragrafoRegistre.style.color = `var(--cor-${textoCor})`;
+    subtitleFerramentas.style.color = `var(--cor-${textoCor})`;
+    paragrafoFerramentas.style.color = `var(--cor-${textoCor})`;
+    bgColor.style.backgroundColor = `var(--cor-${cor})`;
+};
 
 btnNoturno.addEventListener('click', () => {
-    if (noturno) {
-        header.style.backgroundColor  = 'var(--cor-branco)'
-        header.style.boxShadow = '2px 2px 8px rgba(0, 0, 0, 0.070)'
-        main.style.backgroundColor  = 'var(--cor-branco)'
-        footer.style.backgroundColor  = 'var(--cor-branco)'
-        login.style.color = 'var(--cor-dark)'
-        titleComece.style.color = 'var(--cor-dark)'
-        paragrafoComece.style.color = 'var(--cor-dark)'
-        subtitleResgistre.style.color = 'var(--cor-dark)'
-        paragrafoRegistre.style.color = 'var(--cor-dark)'
-        subtitleFerramentas.style.color = 'var(--cor-dark)'
-        paragrafoFerramentas.style.color = 'var(--cor-dark)'
-        bgColor.style.backgroundColor = 'var(--cor-branco)'
-        
-    } else {
-        header.style.backgroundColor  = 'var(--cor-dark)'
-        header.style.boxShadow = '2px 2px 10px #f7f7f720'
-        main.style.backgroundColor  = 'var(--cor-dark)'
-        footer.style.backgroundColor  = 'var(--cor-dark)'
-        login.style.color = 'var(--cor-branco)'
-        titleComece.style.color = 'var(--cor-branco)'
-        paragrafoComece.style.color = 'var(--cor-branco)'
-        subtitleResgistre.style.color = 'var(--cor-branco)'
-        paragrafoRegistre.style.color = 'var(--cor-branco)'
-        subtitleFerramentas.style.color = 'var(--cor-branco)'
-        paragrafoFerramentas.style.color = 'var(--cor-branco)'
-        bgColor.style.backgroundColor = 'var(--cor-dark)'
-    }
-    noturno = !noturno
-})
+    noturno = !noturno;
+    modoNoturno();
+});
+
