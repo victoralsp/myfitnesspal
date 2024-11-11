0ed9cb22bb0fd05ece781281f48dcf7b3c71ff81
Victor Hugo Alves Pinheiro <victorpinheiro1008@gmail.com>
2024-11-11 10:02:54 -0300

    Correção de cor do resultado do calc imc

--- js/index.js
+++ js/index.js
@@ -1,30 +1,31 @@
 // Section de calcular imc 
-const resultado = document.querySelector('#resultado')
-const altura = document.querySelector('#altura')
-const peso = document.querySelector('#peso')
+const resultado = document.getElementById('resultado')
+const altura = document.getElementById('altura')
+const peso = document.getElementById('peso')
+const btnCalcular = document.getElementById('btnCalcular')
 
-const calcularIMC = () => {
+btnCalcular.addEventListener('click', ()=> {
     if (altura.value !== '' && peso.value !== '') {
-         const imc = (peso.value / (altura.value * altura.value)).toFixed(2)
-        let classificacao = ''
-        if (imc < 18.5) {
-            classificacao = 'Abaixo do peso'
-        } else if (imc < 25) {
-            classificacao = 'Peso Normal'
-        } else if (imc < 30) {
-            classificacao = 'Acima do peso'
-        } else if (imc < 40) {
-            classificacao = 'Obesidade grau II'
-        } else {
-            classificacao = 'Obesidade grau III'
-        }
+        const imc = (peso.value / (altura.value * altura.value)).toFixed(2)
+       let classificacao = ''
+       if (imc < 18.5) {
+           classificacao = 'Abaixo do peso'
+       } else if (imc < 25) {
+           classificacao = 'Peso Normal'
+       } else if (imc < 30) {
+           classificacao = 'Acima do peso'
+       } else if (imc < 40) {
+           classificacao = 'Obesidade grau II'
+       } else {
+           classificacao = 'Obesidade grau III'
+       }
 
-        resultado.innerHTML = `IMC: ${imc} (${classificacao})`
-        }
-        else {
-            resultado.innerHTML = 'Verifique se os campos foram preenchidos corretamente.'
-        }
-}
+       resultado.innerHTML = `IMC: ${imc} (${classificacao})`
+       }
+       else {
+           resultado.innerHTML = 'Verifique se os campos foram preenchidos corretamente.'
+       }
+})
 
 // Modo noturno 
 
