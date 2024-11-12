f9cbccb1f3d12735949a8fe84a125616c8cd5929
Victor Hugo Alves Pinheiro <victorpinheiro1008@gmail.com>
2024-11-12 11:55:05 -0300

    Funcionalidade: imagem some ao pesquisar alimento, se o alimento nao for encontrado ou a pesquisa for apagada, a imagem volta

--- js/calorias-alimentos.js
+++ js/calorias-alimentos.js
@@ -1,43 +1,52 @@
 const divAlimentos = document.getElementById('alimentos')
 const inputBuscaAlimento = document.getElementById('buscar-alimento')
 
-fetch('json/alimentos.json').then((resposta) => {
-    return resposta.json()
-}).then((informacoes) => {
-    informacoes.alimentos.map((listagem) => {
-        divAlimentos.innerHTML += 
-        `
-        <div class="alimentos-calorias">
-        <h1>${listagem.nome}</h1>
-        <p>Calorias: <span class="valores-span">${listagem.calorias}</span></p> 
-        <p>Proteína: <span class="valores-span">${listagem.proteinas}</span></p> 
-        <p>Gordura: <span class="valores-span">${listagem.gorduras}</span></p> 
-        <p>Carboidratos: <span class="valores-span">${listagem.carboidratos}</span></p> 
-        <p>Quantidade por porção: <span class="valores-span">${listagem.quantidade_porcao}</span></p></div>
-        `
-    })
+    fetch('json/alimentos.json').then((resposta) => {
+        return resposta.json()
+    }).then((informacoes) => {
+        informacoes.alimentos.map((listagem) => {
+            divAlimentos.innerHTML += 
+            `
+            <div class="alimentos-calorias">
+            <h1>${listagem.nome}</h1>
+            <p>Calorias: <span class="valores-span">${listagem.calorias}</span></p> 
+            <p>Proteína: <span class="valores-span">${listagem.proteinas}</span></p> 
+            <p>Gordura: <span class="valores-span">${listagem.gorduras}</span></p> 
+            <p>Carboidratos: <span class="valores-span">${listagem.carboidratos}</span></p> 
+            <p>Quantidade por porção: <span class="valores-span">${listagem.quantidade_porcao}</span></p></div>
+            `
+        })
 
     // Verifica se há algum alimento com nome igual ao pesquisado
     inputBuscaAlimento.addEventListener('input', () => {
         const valorBuscaInput = inputBuscaAlimento.value.toLowerCase()
         const alimentosDivs = document.querySelectorAll('.alimentos-calorias')
         const errosBusca = document.getElementById('erros')
-
+        const imgComidas = document.getElementById('img-comidas-saudaveis')
         let alimentoEncontrado = false
-    
+
+        // Remove a imagem caso algum alimento seja pesquisado
+        if (alimentoEncontrado || valorBuscaInput === '') {           
+            imgComidas.style.display = 'block'
+        } else {    
+            imgComidas.style.display = 'none'
+        } 
+        
+        // Percorre o objeto e verifica se o item pesquisado está presente no json 
         alimentosDivs.forEach(div => {
             const nomeAlimento = div.querySelector('h1').textContent.toLowerCase()
 
-            if (nomeAlimento.includes(valorBuscaInput)) {
+            if (nomeAlimento.includes(valorBuscaInput) && valorBuscaInput) {
                 div.style.display = 'block'
                 alimentoEncontrado = true
             } else {
                 div.style.display = 'none'
             }
         })
+        
         if (!alimentoEncontrado && valorBuscaInput) {
-            // alert('teste')
             errosBusca.innerHTML = 'Alimento não encontrado, verifique se digitou corretamente.'
+            imgComidas.style.display = 'block'
         } else {
             errosBusca.innerHTML = ''
         }
