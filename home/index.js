496b840d44511d552f2c0f70cafe94e2f8a57d0a
Victor Hugo Alves Pinheiro <victorpinheiro1008@gmail.com>
2025-01-12 15:24:07 -0300

    Alterando animações no scroll do css pelo método com javascript

--- home/index.js
+++ home/index.js
@@ -12,6 +12,25 @@ btnMenu.addEventListener('click', () => {
 })
 
 
+// Efeitos de animações no scroll 
+const myObserver = new IntersectionObserver((entries) => {
+    entries.forEach((entry) => {
+        if(entry.isIntersecting) {
+            entry.target.classList.add('show')
+        } else {
+            entry.target.classList.remove('show')
+        }
+    })
+})
+
+const elements = document.querySelectorAll('.hidden')
+elements.forEach((element) => myObserver.observe(element))
+
+
+
+
+
+
 
 
 
